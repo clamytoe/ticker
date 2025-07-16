acd8bb1dcf779321bb50a4d79ff0f703c35a80a8
Martin Uribe <clamytoe@gmail.com>
2025-07-16 09:01:39 -0500

    Modified decimals returned

--- ticker/stock.py
+++ ticker/stock.py
@@ -5,14 +5,14 @@ def get_stock_info(ticker):
     t = yf.Ticker(ticker)
     info = t.info
     price: float = info.get("regularMarketPrice", "0.0")
-    prev_close = info.get("regularMarketPreviousClose", price)
+    prev_close: float = info.get("regularMarketPreviousClose", price)
     change = price - prev_close
     percent = (change / prev_close) * 100 if prev_close else 0
     name = info.get("longName", ticker)
     return {
         "name": name,
         "price": price,
-        "change": change,
-        "percent": percent,
+        "change": f"{change:.6f}",
+        "percent": f"{percent:.6f}",
         "logo_url": info.get("logo_url", f"{ticker.upper()}"),
     }

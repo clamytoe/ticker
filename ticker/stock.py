fe19394b54920b7db5e7dec0ef7b969194f4dda9
Martin Uribe <clamytoe@gmail.com>
2025-07-16 10:04:05 -0500

    Modified code to use text logo if image was not found

--- ticker/stock.py
+++ ticker/stock.py
@@ -4,15 +4,20 @@ import yfinance as yf
 def get_stock_info(ticker):
     t = yf.Ticker(ticker)
     info = t.info
-    price: float = info.get("regularMarketPrice", "0.0")
-    prev_close: float = info.get("regularMarketPreviousClose", price)
+    price = info.get("regularMarketPrice")
+    prev_close = info.get("regularMarketPreviousClose", price)
     change = price - prev_close
     percent = (change / prev_close) * 100 if prev_close else 0
     name = info.get("longName", ticker)
+    logo_url = info.get("logo_url")
+    symbol = ticker.upper()
+    if not logo_url:
+        logo_url = None  # or symbol, but now app uses symbol as fallback if no logo_url
     return {
+        "symbol": symbol,
         "name": name,
         "price": price,
         "change": change,
         "percent": percent,
-        "logo_url": info.get("logo_url", f"{ticker.upper()}"),
+        "logo_url": logo_url,
     }
